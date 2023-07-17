04bd2bbb89745cdae84ced55f9c5db8ccb07911d
John Crawley <JohnMichaelCrawley@outlook.com>
2023-07-17 23:26:18 +0100

    Initial page design skeleton
    * Added a homepage to the initial page users go on
    * Created a basic HTML structure for further development
    * Added custom CSS rules to the page
    * Created a basic HTML / CSS template for the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Set the main page for the website by
+  # setting a GET request for the index 
+
+  # Root the main page to home.erb 
+  root 'pages#home'
+
+
 end
