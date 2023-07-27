94d20feecb0281194311245c4520dd313dcba1c2
John Crawley <JohnMichaelCrawley@outlook.com>
2023-07-27 03:24:35 +0100

    Updated The Upvote / Downvote System
    I updated the code for the voting system so now, you don't need two votes to boost a post, if a post has even one upvote it will boost the post up.

--- lib/newsSharerCustomFeedGem/lib/newsSharerCustomFeedGem.rb
+++ lib/newsSharerCustomFeedGem/lib/newsSharerCustomFeedGem.rb
@@ -5,17 +5,18 @@ module NewsSharerCustomFeedGem
   # This code orders posts by the weight of the post
   module PostOrdering
       def posts_ordered_by_upvotes_and_downvotes
-        # Fetch all posts within the last 24 hours
-        recent_posts = Post.where('created_at >= ?', 24.hours.ago)
-        # Order the posts by the most upvotes and the least downvotes
-        sorted_posts = recent_posts.sort_by do |post|
-          upvotes_count = post.get_upvotes.size
-          downvotes_count = post.get_downvotes.size
-          [upvotes_count, downvotes_count]
-        end.reverse
-
-        # Return the sorted posts
-        sorted_posts
+           # Order the posts by the most upvotes (descending) and the least downvotes (ascending)
+           sorted_posts = recent_posts.sort_by do |post|
+            upvotes_count = post.get_upvotes.size
+            downvotes_count = post.get_downvotes.size
+    
+            # Prioritize posts with more upvotes by multiplying the upvotes by -1 (to sort in descending order)
+            # and adding the downvotes (to break ties in favor of posts with more downvotes)
+            [-upvotes_count, downvotes_count]
+          end
+    
+          # Return the sorted posts
+          sorted_posts
     end
     class Error < StandardError; end
   end
